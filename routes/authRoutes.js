c6c13e7ac33e5b945061c72688e62bcbde7e3fb5
PhillipeAlves <phillipe.alves@gmail.com>
2020-10-09 14:03:59 +1100

    redirect

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,36 +1,36 @@
-const passport = require("passport");
+const passport = require('passport')
 
-module.exports = (app) => {
-  app.get("/auth/google", function (request, response, next) {
-    passport.authenticate("google", { scope: ["profile", "email"] })(
+module.exports = app => {
+  app.get('/auth/google', function (request, response, next) {
+    passport.authenticate('google', { scope: ['profile', 'email'] })(
       request,
       response,
       next
-    );
-  });
+    )
+  })
 
   app.get(
-    "/auth/google/callback",
-    passport.authenticate("google"),
+    '/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/' }),
     (req, res) => {
-      res.redirect("/");
+      res.redirect('/recipes')
     }
-  );
+  )
 
   app.get(
-    "/api/auth/google/callback",
-    passport.authenticate("google"),
+    '/api/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/' }),
     (req, res) => {
-      res.redirect("/");
+      res.redirect('/recipes')
     }
-  );
+  )
 
-  app.get("/logout", (req, res) => {
-    req.logout();
-    res.redirect("/");
-  });
+  app.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/')
+  })
 
-  app.get("/current_user", (req, res) => {
-    res.send(req.user);
-  });
-};
+  app.get('/current_user', (req, res) => {
+    res.send(req.user)
+  })
+}
